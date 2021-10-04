b6fb0fdef76251bd120b5a902cb892725c15b151
Luiz Torre <luizmptorre@gmail.com>
2021-10-04 15:27:23 -0300

    configurando o acesso do email por terceiros

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,17 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["dev_email"],
+    password:ENV["dev_password"]
+  }
+  config.action_mailer.default_url_options = { :host => 'http://127.0.0.1:3000' }
+  ActionMailer::Base.raise_delivery_errors = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
